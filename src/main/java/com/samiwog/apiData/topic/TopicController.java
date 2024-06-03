d1a4fb1db666ca23276a1933a1e2eb2ecefb1ddc
Samuel Ogunbodede <samiwogdev@gmail.com>
2024-06-03 10:42:15 +0100

    update made

---
+++ src/main/java/com/samiwog/apiData/topic/TopicController.java
@@ -0,0 +1,53 @@
+package com.samiwog.apiData.topic;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class TopicController {
+	
+	@Autowired
+	private TopicService topicservice;
+
+	@GetMapping("/topics")
+	public List<Topic> getTopics() {
+
+		return topicservice.getAllTopics();
+	}
+
+//	@GetMapping("/topics/{foo}")
+//	public Topic getTopic(@PathVariable("foo") String id) {
+//		return topicservice.getTopic(id);
+//	}
+	
+	@GetMapping("/topics/{id}")
+	public Topic getTopic(@PathVariable String id) {
+		return topicservice.getTopic(id);
+	}
+	
+//	@RequestMapping(method=RequestMethod.POST, value="/topics")
+	@PostMapping("/topics")
+	public void addTopic(@RequestBody Topic topic) {
+		topicservice.addTopic(topic);
+	}
+	
+	@PutMapping("/topics/{id}")
+	public void updateTopic(@RequestBody Topic topic, @PathVariable String id) {
+		topicservice.updateTopic(topic, id);
+	}
+	
+	@DeleteMapping("/topics/{id}")
+	public void deleteTopic(@PathVariable String id) {
+		topicservice.deleteTopic(id);
+	}
+}
